7cb1f52763e448a69658fea2ac0b357e039ec7ae
Hunter <hunter.griffin@monashmotorsport.com>
2024-07-04 23:23:12 +1000

    Set up for responding to param changes in the editor

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -23,7 +23,11 @@ struct CustomRotarySlider : juce::Slider
 //==============================================================================
 /**
 */
-class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor
+class SimpleEQAudioProcessorEditor  : public juce::AudioProcessorEditor,
+    // inherit from audio processor listener 
+juce::AudioProcessorParameter::Listener,  // Listener needs to be threadsafe and fast 
+// inherit timer for atomic counting 
+juce::Timer
 {
 public:
     SimpleEQAudioProcessorEditor (SimpleEQAudioProcessor&);
@@ -33,10 +37,20 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    // From the Listener class
+     void parameterValueChanged(int parameterIndex, float newValue) override;
+
+    virtual void parameterGestureChanged(int parameterIndex, bool gestureIsStarting) override {}
+
+    // From Timer class
+    void timerCallback() override;
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     SimpleEQAudioProcessor& audioProcessor;
+
+    juce::Atomic<bool> parametersChanged{ false };
 
     CustomRotarySlider peakFreqSlider,
         peakGainSlider,
@@ -62,6 +76,7 @@ private:
 
     std::vector<juce::Component*> getComps();
 
+    // Monochain for response curve
     MonoChain MonoChain;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleEQAudioProcessorEditor)
