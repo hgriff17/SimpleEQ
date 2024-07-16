1350a51042ad8c38efbcc1c345e1ca27e0c44071
Hunter <hunter.griffin@monashmotorsport.com>
2024-07-16 22:54:14 +1000

    GUI listens to param changes

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -28,11 +28,26 @@ SimpleEQAudioProcessorEditor::SimpleEQAudioProcessorEditor (SimpleEQAudioProcess
         addAndMakeVisible(comp);
     }
 
+    // add ourselves to the listener of params
+    const auto& params = audioProcessor.getParameters();
+    for (auto param : params)
+    {
+        param->addListener(this);
+    }
+
+    startTimerHz(60);
+
     setSize (600, 400);
 }
 
+// destructor to de-register
 SimpleEQAudioProcessorEditor::~SimpleEQAudioProcessorEditor()
 {
+    const auto& params = audioProcessor.getParameters();
+    for (auto param : params)
+    {
+        param->removeListener(this);
+    }
 }
 
 //==============================================================================
@@ -152,8 +167,14 @@ void SimpleEQAudioProcessorEditor::timerCallback()
 {
     if (parametersChanged.compareAndSetBool(false, true))
     {
+        
         //update the monochain
+        auto chainSettings = getChainSettings(audioProcessor.apvts);
+        auto peakCoefficients = makePeakFilter(chainSettings, audioProcessor.getSampleRate());
+        updateCoefficients(MonoChain.get<ChainPositions::Peak>().coefficients, peakCoefficients);
+         
         //signal a repaint
+        repaint();
     }
 }
 
